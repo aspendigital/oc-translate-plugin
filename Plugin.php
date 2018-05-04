2c19dc40376bc2061cdbf638d84482a86a12a72e
Jim Cottrell <jim@aspendigital.com>
2018-05-04 15:11:11 -0600

    Return a 404 for static pages with no URL defined in the active locale, overriding fallback to default URL

--- Plugin.php
+++ Plugin.php
@@ -2,6 +2,9 @@
 namespace AspenDigital\Translate;
 
 use Event;
+use RainLab\Translate\Classes\Translator;
+use Response;
+use View;
 
 class Plugin extends \System\Classes\PluginBase
 {
@@ -19,6 +22,24 @@ class Plugin extends \System\Classes\PluginBase
 
     public function boot()
     {
+        // RainLab.Translate operates on the assumption that all content has a translation in every
+        // locale, and if not, showing the default locale's content is acceptable. This overrides that
+        // behavior for static pages so if the page doesn't have a URL assigned for the current
+        // locale, it's a 404.
+        Event::listen('cms.page.beforeDisplay', function($controller, $url, $page) {
+            if (!$page || $url === '/') {
+                return;
+            }
+
+            $staticPage = array_get($page->apiBag, 'staticPage');
+            $locale = Translator::instance()->getLocale();
+
+            if ($staticPage && !$staticPage->hasTranslatablePageUrl($locale)) {
+                $page404 = $controller->getRouter()->findByUrl('/404');
+                return $page404 ?: Response::make(View::make('cms::404'), 404);
+            }
+        });
+
         if (!$this->app->runningInBackend()) {
             return;
         }
--- README.md
+++ README.md
@@ -6,3 +6,6 @@ This plugin registers a component named `homeLocalePicker` that can be used in p
 
 ## Static Pages Master Locale Switcher
 When editing static pages, you will see a `Change Locale` button in the form toolbar. This can be used to change the locale of all translatable fields in the page form.
+
+## Static Page Locale Routing
+RainLab.Translate falls back to the default locale URL if unassigned in the active locale. This plugin overrides that behavior to return a 404 if a static page does not have a locale URL defined.
